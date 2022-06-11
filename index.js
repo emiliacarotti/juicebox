50c208762dca0f078713c91a7577f9208cae860d
emiliacarotti <emiliac@vt.edu>
2022-06-11 11:54:32 -0400

    end of part 1

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 /// this main application file will use functions from db/index.js
 /// it will also listen to the front-end code making AJAX requests to certain routes
 /// and make our own requests to the database
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+// morgan and json stuff
+const morgan = require('morgan');
+server.use(morgan('dev'));
+server.use(express.json())
+
+
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
